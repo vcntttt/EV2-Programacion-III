1bf904eca37c67c5a73d873e5da8403db62f7c8c
Vicente Rivera <vicenterivera225@gmail.com>
2024-06-06 17:38:21 -0400

    base

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hola")
+import tkinter as tk
+
+class Interfaz():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("8 - Gestor de Tareas con Lista Enlazada")
+        self.root.geometry("500x400")
+        self.root.resizable(False, False)
+
+root = tk.Tk()
+app = Interfaz(root)
+root.mainloop()
